5f1f95189d5d917ab9484ad0f8dc1cddb745d0fb
MetaGS <munar93@mail.ru>
2021-03-18 19:37:26 +0600

    init

---
+++ buttonWithShineOnHover/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = (props) => {
+  return (
+    <div className="wrapper">
+      <button className="but">This is A Button</button>
+    </div>
+  );
+};
+
+export default Index;
